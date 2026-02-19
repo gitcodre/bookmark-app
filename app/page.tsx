ba887d750f3890ac7b2f3b03b80d29dfb7a967c5
gitcodre <tiwarianimesh14@gmail.com>
2026-02-19 20:43:26 +0530

    fix oauth redirect

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${location.origin}/auth/callback`,
       },
     });
   };
